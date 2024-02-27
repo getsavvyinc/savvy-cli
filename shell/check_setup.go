ea18ea15a519d84e4470832bd651fdc8f8ce457b
Shantanu <joshishantanu4@gmail.com>
2024-02-26 16:42:33 -0800

    kind: extract shell/kind pkg for reuse

--- shell/check_setup.go
+++ shell/check_setup.go
@@ -1,5 +1,9 @@
 package shell
 
+import (
+	"github.com/getsavvyinc/savvy-cli/shell/kind"
+)
+
 type SetupChecker interface {
 	// CheckSetup returns an non nil error if the shell setup is not correct
 	// The error message should contain instructions on how to fix the setup and is safe to display to the user
@@ -9,9 +13,9 @@ type SetupChecker interface {
 func NewSetupChecker() SetupChecker {
 	shell := detectWithDefault()
 	switch shell {
-	case Zsh:
+	case kind.Zsh:
 		return &zshSetupChecker{}
-	case Bash:
+	case kind.Bash:
 		return &bashSetupChecker{}
 	default:
 		return &nopSetupChecker{}
