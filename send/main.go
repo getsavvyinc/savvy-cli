49d793bf7042bc9721ee69376b8ebf348ebceb34
Shantanu <joshishantanu4@gmail.com>
2024-03-08 15:00:37 -0800

    add send

---
+++ send/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args[1:]
+	f, err := os.OpenFile("/Users/shantanu/.savvy_history", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		panic(err)
+	}
+	h := slog.NewTextHandler(f, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	})
+	logger := slog.New(h)
+	logger.Debug("send called")
+	defer f.Close()
+
+	socketPath := os.Getenv("SAVVY_SOCKET_PATH")
+	if socketPath == "" {
+		socketPath = "/tmp/savvy-socket"
+		// err := fmt.Errorf("cannot record commands: SAVVY_SOCKET_PATH is not set")
+		// display.ErrorWithSupportCTA(err)
+		// return
+	}
+	logger.Debug("dialing socket", "socketPath", socketPath)
+	conn, err := net.Dial("unix", socketPath)
+	if err != nil {
+		err = fmt.Errorf("failed to record command: %v", err)
+		return
+	}
+	defer conn.Close()
+	message := strings.Join(args[:], " ") // ["echo hello world"] -> "echo hello world"
+	if len(message) == 0 {
+		// nothing to do.
+		return
+	}
+	logger.Debug("writing to socket", "message", message)
+	if _, err = io.WriteString(conn, message+"\n"); err != nil {
+		err = fmt.Errorf("failed to record command locally: %v", err)
+		return
+	}
+	logger.Debug("finished writing to socket", "message", message)
+}
