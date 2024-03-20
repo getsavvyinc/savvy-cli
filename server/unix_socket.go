06615d1a862e31935f7f52b26ada3ff8962080b8
Shantanu <joshishantanu4@gmail.com>
2024-03-20 16:06:19 -0700

    server: record command exit status and add support for ignoring errors

--- server/unix_socket.go
+++ server/unix_socket.go
@@ -1,22 +1,27 @@
 package server
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"log/slog"
 	"net"
 	"os"
+	"strings"
 	"sync"
 	"sync/atomic"
 )
 
 type UnixSocketServer struct {
-	socketPath string
-	listener   net.Listener
+	socketPath   string
+	logger       *slog.Logger
+	listener     net.Listener
+	filterErrors bool
 
 	mu                  sync.Mutex
-	commands            []string
+	commands            []*RecordedData
+	lookupCommand       map[string]*RecordedData
 	commandRecordedHook func(string)
 
 	closed atomic.Bool
@@ -33,6 +38,20 @@ func WithCommandRecordedHook(hook func(string)) Option {
 		s.commandRecordedHook = hook
 	}
 }
+
+func WithLogger(logger *slog.Logger) Option {
+	return func(s *UnixSocketServer) {
+		s.logger = logger
+	}
+}
+
+func WithFilterErrors(filter bool) Option {
+	return func(s *UnixSocketServer) {
+		s.filterErrors = filter
+	}
+}
+
+var defaultLogger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
 
 func NewUnixSocketServerWithDefaultPath(opts ...Option) (*UnixSocketServer, error) {
 	return NewUnixSocketServer(defaultSocketPath, opts...)
@@ -54,6 +73,7 @@ func newUnixSocketServer(socketPath string, opts ...Option) (*UnixSocketServer, 
 	srv := &UnixSocketServer{
 		socketPath: socketPath,
 		listener:   listener,
+		logger:     defaultLogger,
 	}
 
 	for _, opt := range opts {
@@ -67,7 +87,15 @@ func (s *UnixSocketServer) Commands() []string {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	return s.commands
+	var commands []string
+
+	for _, cmd := range s.commands {
+		if s.filterErrors && len(cmd.ExitStatus) > 0 && cmd.ExitStatus != "0" {
+			continue
+		}
+		commands = append(commands, cmd.Command)
+	}
+	return commands
 }
 
 func (s *UnixSocketServer) Close() error {
@@ -95,6 +123,12 @@ func (s *UnixSocketServer) ListenAndServe() {
 	}
 }
 
+type RecordedData struct {
+	Command    string `json:"command"`
+	StepID     string `json:"step_id"`
+	ExitStatus string `json:"exit_status"`
+}
+
 func (s *UnixSocketServer) handleConnection(c net.Conn) {
 	defer c.Close()
 
@@ -103,10 +137,20 @@ func (s *UnixSocketServer) handleConnection(c net.Conn) {
 		fmt.Printf("Failed to read from connection: %s\n", err)
 		return
 	}
-	command := string(bs)
-	s.appendCommand(command)
-	if s.commandRecordedHook != nil {
-		s.commandRecordedHook(command)
+
+	var data RecordedData
+	if err := json.Unmarshal(bs, &data); err != nil {
+		s.logger.Debug("Failed to unmarshal data", "error", err.Error(), "component", "server", "input", string(bs))
+		return
+	}
+
+	if s.maybeAppendData(data) && s.commandRecordedHook != nil {
+		s.commandRecordedHook(data.Command)
+	}
+
+	if data.Command != "" && data.ExitStatus != "0" {
+		s.logger.Debug("command failed", "command", data.Command, "exit_status", data.ExitStatus)
+		s.updateExitStatus(data.StepID, data.ExitStatus)
 	}
 }
 
@@ -114,9 +158,30 @@ func (s *UnixSocketServer) SocketPath() string {
 	return s.socketPath
 }
 
-func (s *UnixSocketServer) appendCommand(command string) {
+func (s *UnixSocketServer) updateExitStatus(stepID, exitStatus string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	s.commands = append(s.commands, command)
+	if cmd, ok := s.lookupCommand[stepID]; ok {
+		cmd.ExitStatus = exitStatus
+	}
+}
+
+func (s *UnixSocketServer) maybeAppendData(data RecordedData) bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	cmd := strings.TrimSpace(data.Command)
+
+	if cmd == "" {
+		return false
+	}
+
+	if _, ok := s.lookupCommand[data.StepID]; ok {
+		return false
+	}
+
+	s.commands = append(s.commands, &data)
+	s.logger.Debug("command recorded", "command", data.Command)
+	return true
 }
