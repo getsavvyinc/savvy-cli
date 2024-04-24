b47da3dd0220828d6bd39683941b78aa1c84b308
Shantanu <joshishantanu4@gmail.com>
2024-04-24 13:53:26 -1000

    client: Allow Guest clients
    savvy run will allow anyone to run public runbooks on their local
    terminal.

--- cmd/run.go
+++ cmd/run.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/getsavvyinc/savvy-cli/client"
+	"github.com/getsavvyinc/savvy-cli/display"
 	"github.com/spf13/cobra"
 )
 
@@ -10,13 +12,30 @@ import (
 var runCmd = &cobra.Command{
 	Use:     "run",
 	Short:   "Run takes a runbook ID and runs it",
-	Example: "savvy run $r-runbookID",
+	Example: "savvy run $rb-runbookID",
 	Long:    `Run takes a runbook ID and runs it. Run automatically steps though the runbook for you, there's no need manually copy paste individual commands.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
-	},
+	Run:     savvyRun,
 }
 
 func init() {
 	rootCmd.AddCommand(runCmd)
+}
+
+func savvyRun(cmd *cobra.Command, args []string) {
+	ctx := cmd.Context()
+	logger := loggerFromCtx(ctx).With("command", "run")
+
+	var cl client.Client
+	cl, err := client.New()
+	if err != nil {
+		logger.Debug("error creating client", "error", err, "message", "falling back to guest client")
+		cl = client.Guest()
+	}
+
+	if len(args) == 0 {
+		display.ErrorMsg("missing: runbookID")
+		os.Exit(1)
+	}
+
+	_ = cl
 }
