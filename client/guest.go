aa1ef0e3e64436d3d8f473c1f4dfb7c190449cb7
Shantanu <joshishantanu4@gmail.com>
2024-04-24 15:07:14 -1000

    client: Add separate Guest client for fetching public runbooks

---
+++ client/guest.go
@@ -0,0 +1,90 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/getsavvyinc/savvy-cli/config"
+)
+
+var _ Client = (*guest)(nil)
+
+func NewGuest() Client {
+	return &guest{
+		cl: &http.Client{
+			Transport: &GuestRoundTripper{savvyVersion: config.Version()},
+		},
+		apiHost: config.APIHost(),
+	}
+}
+
+type GuestRoundTripper struct {
+	savvyVersion string
+}
+
+func (g *GuestRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Clone the request to ensure thread safety
+	clonedReq := req.Clone(req.Context())
+	clonedReq.Header.Set("X-Savvy-Version", g.savvyVersion)
+	clonedReq.Header.Set("X-Savvy-Guest", "true")
+
+	// Use the embedded Transport to perform the actual request
+	return http.DefaultTransport.RoundTrip(clonedReq)
+}
+
+type guest struct {
+	cl      *http.Client
+	apiHost string
+}
+
+// apiURL returns the full url to the api endpoint
+// path must start with a slash. e.g. /api/v1/whoami
+// apiURL will add a slash if it's missing
+func (g *guest) apiURL(path string) string {
+	if !strings.HasPrefix(path, "/") {
+		path = "/" + path
+	}
+	return g.apiHost + path
+}
+
+func (g *guest) WhoAmI(ctx context.Context) (string, error) {
+	return "Savvy Guest", nil
+}
+
+var ErrCannotUseGuest = errors.New("cannot use guest client for this operation")
+
+func (g *guest) GenerateRunbookV2(ctx context.Context, commands []RecordedCommand) (*GeneratedRunbook, error) {
+	return nil, fmt.Errorf("%w: %v", ErrCannotUseGuest, "generate runbook")
+}
+
+func (g *guest) GenerateRunbook(ctx context.Context, commands []string) (*GeneratedRunbook, error) {
+	return nil, fmt.Errorf("%w: %v", ErrCannotUseGuest, "generate runbook")
+}
+
+func (g *guest) RunbookByID(ctx context.Context, id string) (*Runbook, error) {
+	cl := g.cl
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, g.apiURL("/api/v1/public/runbook"), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	qp := req.URL.Query()
+	qp.Set("runbook_id", id)
+	req.URL.RawQuery = qp.Encode()
+
+	resp, err := cl.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var runbook Runbook
+	if err := json.NewDecoder(resp.Body).Decode(&runbook); err != nil {
+		return nil, err
+	}
+	return &runbook, nil
+}
