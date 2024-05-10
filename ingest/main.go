fbc9dd42661568a4e0f3fc216eaea5cb5250fe85
Shantanu <joshishantanu4@gmail.com>
2024-05-09 18:26:27 -0700

    generate embedding for cheatsheets after parsing them

--- ingest/main.go
+++ ingest/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io/fs"
 	"log/slog"
+	"os"
 	"path/filepath"
+	"strings"
+	"sync"
 
 	"golang.org/x/sync/errgroup"
 
 	"github.com/getsavvyinc/savvy-cli/ingest/cheatsheet"
 	"github.com/getsavvyinc/savvy-cli/ingest/cheatsheet/parser"
+	"github.com/getsavvyinc/savvy-cli/ingest/llm"
 )
 
 const tldrPath = "tldr/pages/"
@@ -19,10 +24,22 @@ const maxConcurrency = 500
 
 func main() {
 	logger := slog.Default()
+	ctx := context.Background()
+
+	authToken := os.Getenv("OPENAI_API_KEY")
+	if authToken == "" {
+		logger.Error("OPENAI_API_KEY is required")
+		return
+	}
+
+	llmClient := llm.NewOpenAIClient(authToken)
 	cheatsheetParser := parser.New(cheatsheet.TLDR)
 
 	var g errgroup.Group
 	g.SetLimit(maxConcurrency)
+
+	var mu sync.Mutex
+	var cheatsheets []*cheatsheet.CheatSheet
 
 	err := filepath.Walk(tldrPath, func(path string, info fs.FileInfo, err error) error {
 		logger := logger.With("path", path)
@@ -47,6 +64,9 @@ func main() {
 				logger.Warn("cheat sheet is nil")
 				return fmt.Errorf("cheat sheet is nil: %s", path)
 			}
+			mu.Lock()
+			cheatsheets = append(cheatsheets, cheatSheet)
+			mu.Unlock()
 			return nil
 		})
 
@@ -59,5 +79,31 @@ func main() {
 
 	if err := g.Wait(); err != nil {
 		logger.Error(err.Error(), "component", "errgroup")
+	}
+
+	for i, cs := range cheatsheets {
+		if i > 10 {
+			logger.Info("breaking early")
+			return
+		}
+
+		prefix := cs.CommonEmbeddingPrefix()
+		for _, example := range cs.Examples {
+			command := example.Command
+			explanation := example.Explanation
+			if embedding, err := llmClient.CreateEmbeddings(ctx, strings.Join([]string{prefix, explanation}, " ")); err != nil {
+				logger.Error(err.Error(), "component", "llmClient.embed.explanation")
+				continue
+			} else {
+				logger.Info("embedding created", "embedding", embedding[:10])
+			}
+
+			if embedding, err := llmClient.CreateEmbeddings(ctx, strings.Join([]string{command}, " ")); err != nil {
+				logger.Error(err.Error(), "component", "llmClient.embed.command")
+				continue
+			} else {
+				logger.Info("embedding created", "embedding", embedding[:10])
+			}
+		}
 	}
 }
