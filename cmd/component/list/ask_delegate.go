fe176de980977ad02afea06e45f158e2ff02101c
Shantanu <joshishantanu4@gmail.com>
2024-05-13 18:22:09 -0700

    component/list: add custom ask delegate

---
+++ cmd/component/list/ask_delegate.go
@@ -0,0 +1,97 @@
+package list
+
+import (
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	statusMessageStyle = lipgloss.NewStyle().
+		Foreground(lipgloss.AdaptiveColor{Light: "#04B575", Dark: "#04B575"}).
+		Render
+)
+
+func newItemDelegate(keys *delegateKeyMap) list.DefaultDelegate {
+	d := list.NewDefaultDelegate()
+
+	d.UpdateFunc = func(msg tea.Msg, m *list.Model) tea.Cmd {
+		var selectedCommand string
+
+		if i, ok := m.SelectedItem().(Item); ok {
+			selectedCommand = i.Title()
+		} else {
+			return nil
+		}
+
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch {
+			case key.Matches(msg, keys.choose):
+				return HandleSelectedCommand(selectedCommand)
+			}
+		}
+
+		return nil
+	}
+
+	help := []key.Binding{keys.choose}
+
+	d.ShortHelpFunc = func() []key.Binding {
+		return help
+	}
+
+	d.FullHelpFunc = func() [][]key.Binding {
+		return [][]key.Binding{help}
+	}
+
+	return d
+}
+
+type delegateKeyMap struct {
+	choose key.Binding
+}
+
+// Additional short help entries. This satisfies the help.KeyMap interface and
+// is entirely optional.
+func (d delegateKeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{
+		d.choose,
+	}
+}
+
+// Additional full help entries. This satisfies the help.KeyMap interface and
+// is entirely optional.
+func (d delegateKeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{
+			d.choose,
+		},
+	}
+}
+
+func newDelegateKeyMap() *delegateKeyMap {
+	return &delegateKeyMap{
+		choose: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "choose and quit"),
+		),
+	}
+}
+
+func NewAskDelegate() list.DefaultDelegate {
+	return newItemDelegate(newDelegateKeyMap())
+}
+
+func HandleSelectedCommand(selectedCommand string) tea.Cmd {
+	return tea.Sequence(
+		func() tea.Msg {
+			return SelectedCommandMsg{selectedCommand}
+		}, tea.Quit,
+	)
+}
+
+type SelectedCommandMsg struct {
+	Command string
+}
