d85b4440875d8907a4dc0a88be5001f54d37d485
Shantanu <joshishantanu4@gmail.com>
2024-06-12 13:52:42 -0700

    server/run: Update state based on commands

--- server/run/run.go
+++ server/run/run.go
@@ -2,10 +2,14 @@ package run
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"log/slog"
 	"net"
 	"os"
+	"sync"
 	"sync/atomic"
 
 	"github.com/getsavvyinc/savvy-cli/server"
@@ -17,7 +21,10 @@ type RunServer struct {
 	socketPath string
 	logger     *slog.Logger
 	listener   net.Listener
-	commands   []*RunCommand
+
+	mu        sync.Mutex
+	currIndex int
+	commands  []*RunCommand
 
 	closed atomic.Bool
 }
@@ -62,26 +69,109 @@ var defaultLogger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions
 func newRunServer(socketPath string, commands []*RunCommand, opts ...Option) (*RunServer, error) {
 	if fileInfo, _ := os.Stat(socketPath); fileInfo != nil {
 
-		cleanup, cerr := cleanup.GetPermission(mode.Run)
+		cleanupOK, cerr := cleanup.GetPermission(mode.Run)
 		if cerr != nil {
 			return nil, cerr
 		}
 
-		if !cleanup {
+		if !cleanupOK {
 			return nil, server.ErrAbortRecording
 		}
 
 		cleanupSocket(socketPath)
+	}
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create listener: %w", err)
 	}
 
 	rs := &RunServer{
 		socketPath: socketPath,
 		logger:     defaultLogger,
 		commands:   commands,
+		listener:   listener,
 	}
 
 	for _, opt := range opts {
 		opt(rs)
 	}
 	return rs, nil
+}
+
+func (rs *RunServer) Close() error {
+	if rs.closed.Load() {
+		return nil
+	}
+	rs.closed.Store(true)
+	return rs.listener.Close()
+}
+
+func (rs *RunServer) handleConnection(c net.Conn) {
+	defer c.Close()
+
+	bs, err := io.ReadAll(c)
+	if err != nil {
+		rs.logger.Error("failed to read from connection: %s\n", err)
+		return
+	}
+
+	var data RunCommand
+	if err := json.Unmarshal(bs, &data); err != nil {
+		rs.logger.Error("failed to unmarshal data", "error", err.Error(), "component", "run", "input", string(bs))
+		return
+	}
+
+	if data.IsShutdown() {
+		rs.Close()
+		return
+	}
+
+	cmd := data.Command
+	rs.handleCommand(cmd, c)
+}
+
+func (rs *RunServer) handleCommand(cmd string, c net.Conn) {
+	switch cmd {
+	case shutdownCommand:
+		rs.Close()
+	case nextCommand:
+		rs.mu.Lock()
+		rs.currIndex++
+		if rs.currIndex >= len(rs.commands) {
+			rs.currIndex = len(rs.commands) - 1
+		}
+		rs.mu.Unlock()
+	case previousCommand:
+		rs.mu.Lock()
+		rs.currIndex--
+		if rs.currIndex < 0 {
+			rs.currIndex = 0
+		}
+		rs.mu.Unlock()
+	case fetchCommand:
+		rs.mu.Lock()
+		if rs.currIndex >= len(rs.commands) {
+			rs.currIndex = len(rs.commands) - 1
+		}
+		if rs.currIndex < 0 {
+			rs.currIndex = 0
+		}
+		cmd := rs.commands[rs.currIndex]
+		rs.mu.Unlock()
+		rs.logger.Debug("fetching command", "command", cmd)
+		json.NewEncoder(c).Encode(cmd)
+	default:
+		rs.logger.Debug("unknown command", "command", cmd)
+	}
+}
+
+const (
+	shutdownCommand = "savvy shutdown"
+	nextCommand     = "savvy internal next"
+	previousCommand = "savvy internal prev"
+	fetchCommand    = "savvy internal fetch"
+)
+
+func (rc *RunCommand) IsShutdown() bool {
+	return rc.Command == shutdownCommand
 }
