550b4e2cff81b325da308deab43b449df12e67d0
Shantanu <joshishantanu4@gmail.com>
2024-06-13 17:05:07 -0700

    param: dedupe multiple instances of a single param

--- param/param.go
+++ param/param.go
@@ -8,10 +8,15 @@ func Extract(input string) []string {
 	// Define a regular expression to match parameters in the form of <alphanumericchars>
 	matches := paramRegex.FindAllStringSubmatch(input, -1)
 
+	seen := make(map[string]struct{})
 	// Extract the matched parameters
 	var params []string
 	for _, match := range matches {
 		if len(match) >= 1 {
+			if _, ok := seen[match[0]]; ok {
+				continue
+			}
+			seen[match[0]] = struct{}{}
 			params = append(params, match[0])
 		}
 	}
