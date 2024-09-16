a55f500b28b71377ede25d6a4329d6ee9759e364
Shantanu <joshishantanu4@gmail.com>
2024-09-16 15:33:51 -0700

    shell/fish: DRY vendorDir and dataDirs setup

--- shell/fish.go
+++ shell/fish.go
@@ -55,33 +55,48 @@ func init() {
 	fishTemplate = template.Must(template.New("fish").Parse(fishBaseScript))
 }
 
-// Spawn starts a fish shell.
-func (f *fish) Spawn(ctx context.Context) (*exec.Cmd, error) {
+// injectVendorCode injects the vendor code into the fish shell configuration.
+// It returns the parent directory that containsins the fish/vendor_conf.d directory.
+// This directory should be added to the XDG_DATA_DIRS environment variable.
+func (f *fish) injectVendorCode(vendorCode *template.Template) (string, error) {
 	// Create a temporary file to store the script
 	tmpDir, err := os.MkdirTemp("", "savvy-fish-*")
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	fishVendorConfDir := filepath.Join(tmpDir, "fish", "vendor_conf.d")
 	if err := os.MkdirAll(fishVendorConfDir, 0755); err != nil {
-		return nil, err
+		return "", err
 	}
 	fishrc, err := os.CreateTemp(fishVendorConfDir, "savvy-fishrc-*.fish")
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	defer fishrc.Close()
 
-	if err := fishTemplate.Execute(fishrc, f); err != nil {
+	if err := vendorCode.Execute(fishrc, f); err != nil {
+		return "", err
+	}
+	return tmpDir, nil
+}
+
+func addVendorDirToXDGDataDirPath(vendorDir string) string {
+	if dataDirs := os.Getenv("XDG_DATA_DIRS"); len(dataDirs) > 0 {
+		return strings.Join([]string{dataDirs, vendorDir}, ":")
+	}
+	return vendorDir
+}
+
+// Spawn starts a fish shell.
+func (f *fish) Spawn(ctx context.Context) (*exec.Cmd, error) {
+	// Create a temporary file to store the script
+	vendorDir, err := f.injectVendorCode(fishTemplate)
+	if err != nil {
 		return nil, err
 	}
 
-	dataDirs := os.Getenv("XDG_DATA_DIRS")
-	if dataDirs == "" {
-		dataDirs = tmpDir
-	} else {
-		dataDirs = strings.Join([]string{dataDirs, tmpDir}, ":")
-	}
+	dataDirs := addVendorDirToXDGDataDirPath(vendorDir)
+
 	cmd := exec.CommandContext(ctx, f.shellCmd)
 	cmd.Env = append(os.Environ(), "SAVVY_CONTEXT=record", fmt.Sprintf("XDG_DATA_DIRS=%s", dataDirs))
 	cmd.WaitDelay = 500 * time.Millisecond
@@ -97,30 +112,12 @@ func (f *fish) SpawnHistoryExpander(ctx context.Context) (*exec.Cmd, error) {
 }
 
 func (f *fish) SpawnRunbookRunner(ctx context.Context, runbook *client.Runbook) (*exec.Cmd, error) {
-	tmpDir, err := os.MkdirTemp("", "savvy-fish-*")
+	vendorDir, err := f.injectVendorCode(fishTemplate)
 	if err != nil {
 		return nil, err
 	}
-	fishVendorConfDir := filepath.Join(tmpDir, "fish", "vendor_conf.d")
-	if err := os.MkdirAll(fishVendorConfDir, 0755); err != nil {
-		return nil, err
-	}
-	fishrc, err := os.CreateTemp(fishVendorConfDir, "savvy-fishrc-*.fish")
-	if err != nil {
-		return nil, err
-	}
-	defer fishrc.Close()
+	dataDirs := addVendorDirToXDGDataDirPath(vendorDir)
 
-	if err := fishTemplate.Execute(fishrc, f); err != nil {
-		return nil, err
-	}
-
-	dataDirs := os.Getenv("XDG_DATA_DIRS")
-	if dataDirs == "" {
-		dataDirs = tmpDir
-	} else {
-		dataDirs = strings.Join([]string{dataDirs, tmpDir}, ":")
-	}
 	cmd := exec.CommandContext(ctx, f.shellCmd)
 	cmd.Env = append(os.Environ(), "SAVVY_CONTEXT=run", fmt.Sprintf("XDG_DATA_DIRS=%s", dataDirs))
 	cmd.Env = append(cmd.Env, runbookRunMetadata(runbook, f)...)
