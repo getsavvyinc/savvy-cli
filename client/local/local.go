fe6be903701e6ba39d1b7e9400c990fedd5ecb27
Shantanu <joshishantanu4@gmail.com>
2024-11-05 19:29:06 -0800

    client: Add a local client.
    Needs to be its own pkg to avoid circular imports

---
+++ client/local/local.go
@@ -0,0 +1,49 @@
+package local
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/getsavvyinc/savvy-cli/client"
+	"github.com/getsavvyinc/savvy-cli/storage"
+)
+
+func New() client.RunbookClient {
+	return &local{}
+}
+
+type local struct{}
+
+var ErrNotFound = errors.New("not found")
+
+func (l *local) RunbookByID(ctx context.Context, id string) (*client.Runbook, error) {
+	rbs, err := storage.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	rb, ok := rbs[id]
+	if !ok {
+		err = fmt.Errorf("runbook %s: %w", id, ErrNotFound)
+		return nil, err
+	}
+	return rb, nil
+}
+
+func (l *local) Runbooks(ctx context.Context) ([]client.RunbookInfo, error) {
+
+	rbs, err := storage.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	var rbis []client.RunbookInfo
+	for id, rb := range rbs {
+		rbis = append(rbis, client.RunbookInfo{
+			RunbookID: id,
+			Title:     rb.Title,
+		})
+	}
+	return rbis, nil
+}
