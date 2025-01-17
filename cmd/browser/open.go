8c11579aacd671d2a319a454e1f69c86ac62cb2f
Shantanu <joshishantanu4@gmail.com>
2025-01-16 19:15:01 -0800

    refactor browser to open chrome for extension
    Signed-off-by: Shantanu <joshishantanu4@gmail.com>

--- cmd/browser/open.go
+++ cmd/browser/open.go
@@ -23,9 +23,13 @@ func OpenCmd(url string) *exec.Cmd {
 
 func Open(url string) {
 	display.Info("Opening your default browser to " + url)
-	var browserOpenError = fmt.Errorf("couldn't open your default browser. Please visit %s in your browser", url)
 
 	cmd := OpenCmd(url)
+	runOpenCmd(cmd, url)
+}
+
+func runOpenCmd(cmd *exec.Cmd, target string) {
+	var browserOpenError = fmt.Errorf("couldn't open your default browser. Please visit %s in your browser", target)
 	if cmd == nil {
 		display.Error(browserOpenError)
 		return
@@ -35,4 +39,11 @@ func Open(url string) {
 		display.Error(browserOpenError)
 		return
 	}
+}
+
+func OpenExtensionSidePanel() {
+	extensionURL := "chrome-extension://jocphfjphhfbdccjfjjnbcnejmbojjlh/side-panel/index.html"
+	display.Info("Opening Savvy's extension on Chrome...")
+	cmd := exec.Command("chrome", extensionURL)
+	runOpenCmd(cmd, extensionURL)
 }
