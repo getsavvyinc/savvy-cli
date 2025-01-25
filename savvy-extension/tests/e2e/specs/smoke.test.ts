fcfa1621c89b1ef511ff0d0da2660f37a068a7bb
Shantanu <joshishantanu4@gmail.com>
2025-01-24 16:40:35 -0800

    rm smoke.test.ts; not requires

--- savvy-extension/tests/e2e/specs/smoke.test.ts
+++
@@ -1,7 +0,0 @@
-describe('The example page can be loaded', () => {
-  it('should be able to go to example page', async () => {
-    await browser.url('https://www.example.com');
-
-    await expect(browser).toHaveTitle('Example Domain');
-  });
-});
